8d90206a1139ea89004dd005ae93ac9c9c96b32d
ASHRAF1925 <1925ashraf@gmail.com>
2022-11-09 16:40:15 +0600

    user review done

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,11 +1,34 @@
-import React from "react";
+import userEvent from "@testing-library/user-event";
+import React, { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Contexts/UserContext";
 import StarRating from "../Common_Components/StarRating/StarRating";
 import "./ServiceDetails.css";
+import { Button } from "flowbite-react";
 
 const ServiceDetails = () => {
   const Singleservice = useLoaderData();
-  console.log(Singleservice);
+  const [reviews, setReviews] = useState();
+  const { user, logout } = useContext(AuthContext);
+
+  const [reviwavialable, setReviewavailable] = useState(false);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/services/reviews/${Singleservice._id}`)
+      .then((response) => response.json())
+
+      .then((result) => {
+        setReviews(result);
+        if (result.length === 0) {
+          console.log("nothing found");
+        } else {
+          setReviewavailable(true);
+        }
+
+        console.log(result);
+      });
+  }, []);
+
   return (
     <div className="container mx-auto ">
       <div className="container mx-auto relative">
@@ -50,9 +73,9 @@ const ServiceDetails = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span>Professional Photographers</span>
@@ -65,9 +88,9 @@ const ServiceDetails = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span>Photo Editing</span>
@@ -80,9 +103,9 @@ const ServiceDetails = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span>
@@ -102,21 +125,58 @@ const ServiceDetails = () => {
           Average User Rating
         </p>
         <hr className="my-4 w-2/4 h-1 ml-auto bg-gray-100 rounded border-0 md:my-10 dark:bg-gray-700" />
-        <p className="text-3xl">
+        <div className="text-3xl">
           {" "}
           <StarRating></StarRating>
-        </p>
+        </div>
       </div>
       {/* Average User Rating End */}
 
       {/* Review Section Start */}
 
       <div className="bg-blue-700 pt-20 pb-10">
-        <h1 className="text-center text-5xl font-bold text-white">User's Review</h1>
+        <h1 className="text-center text-5xl font-bold text-white">
+          User's Review
+        </h1>
         <hr className="my-4 w-3/4 h-1 mx-auto bg-gray-100 rounded border-0 md:my-10 dark:bg-gray-700" />
       </div>
+      {reviwavialable ? (
+        // {/* if review found start */}
+        <div>
+          <h1>{reviews.length}</h1>
+        </div>
+      ) : (
+        // {/* if review found end */}
+        <div className="bg-red-600 text-center text-5xl font-bold text-white p-10">
+          <h1>No Review Avialable Add Some Review</h1>
+        </div>
+      )}
 
       {/* Review Section end */}
+
+      {/* review giving section start */}
+
+      <div className="bg-green-300 pt-20 pb-10">
+        <h1 className="ml-10 text-left text-5xl font-bold ">Add Review</h1>
+        <hr className="my-4 w-3/4 h-1  bg-gray-100 rounded border-0 md:my-10 dark:bg-gray-700" />
+        {user?.uid ? (
+          // {/* if review found start */}
+          <div>
+            <h1>user presenrt</h1>
+          </div>
+        ) : (
+          // {/* if review found end */}
+          <div className="text-center text-3xl font-bold p-10">
+            <h1>
+            To Add Review
+      
+              <Button className="mx-auto mt-4" gradientMonochrome="info">Log In</Button> 
+            </h1>
+          </div>
+        )}
+      </div>
+
+      {/* review giving section end */}
     </div>
   );
 };
